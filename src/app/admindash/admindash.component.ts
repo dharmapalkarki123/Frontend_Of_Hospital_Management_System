347e6b1ae25be453c7c5a9f09767397f5d74946c
unknown <you@example.com>
2024-08-22 22:53:32 +0545

    Initial commit

---
+++ src/app/admindash/admindash.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { Patient } from '../patient';
+import { PatientService } from '../patient.service';
+
+@Component({
+  selector: 'app-admindash',
+  templateUrl: './admindash.component.html',
+  styleUrls: ['./admindash.component.css']
+})
+export class AdmindashComponent {
+  patients:Patient[]=[]
+  constructor(private patientService:PatientService){}
+  ngOnInit():void{
+    this.getPatients();
+  }
+  getPatients(){
+    this.patientService.getPatientList().subscribe(data=>{
+      console.log(data); 
+     this.patients=data;
+    })
+  }
+  
+}
